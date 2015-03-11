0d86a1559e8755dd1efe2e5034b4fea9f1d8c61b
raydelto <raydelto@gmail.com>
2015-03-10 21:50:14 -0400

    First commit of the 3.4 branch, still blank template

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,12 +1,7 @@
 #include "AppDelegate.h"
-#include "GameScene.h"
-#include "../Utilities/ns.h"
-#include "SimpleAudioEngine.h"
+#include "HelloWorldScene.h"
 
-#define FPS 60
-
-USING_NS_CATS;
-
+USING_NS_CC;
 
 AppDelegate::AppDelegate() {
 
@@ -16,88 +11,53 @@ AppDelegate::~AppDelegate()
 {
 }
 
+//if you want a different context,just modify the value of glContextAttrs
+//it will takes effect on all platforms
+void AppDelegate::initGLContextAttrs()
+{
+    //set OpenGL context attributions,now can only set six attributions:
+    //red,green,blue,alpha,depth,stencil
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+
+    GLView::setGLContextAttrs(glContextAttrs);
+}
+
 bool AppDelegate::applicationDidFinishLaunching() {
     // initialize director
-    cocos2d::CCDirector* pDirector = cocos2d::CCDirector::sharedDirector();
-    cocos2d::CCEGLView* pEGLView = cocos2d::CCEGLView::sharedOpenGLView();
+    auto director = Director::getInstance();
+    auto glview = director->getOpenGLView();
+    if(!glview) {
+        glview = GLViewImpl::create("My Game");
+        director->setOpenGLView(glview);
+    }
 
-    pDirector->setOpenGLView(pEGLView);
-	
     // turn on display FPS
-    pDirector->setDisplayStats(false);
+    director->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
-    pDirector->setAnimationInterval(1.0 / FPS);
-    
-    cocos2d::CCSize screenSize = cocos2d::CCEGLView::sharedOpenGLView()->getFrameSize();
-    //CCSize designSize = CCSizeMake(480, 320);
-    cocos2d::CCSize designSize = cocos2d::CCSizeMake(360, 720);
-    std::vector<std::string> searchPaths;
+    director->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    cocos2d::CCScene *pScene = GameScene::scene();
-    
-    cocos2d::CCLog("Screen width = %f height = %f\n", screenSize.width, screenSize.height);
-
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-	//IOS Specific Settings
-	// OpenGLView initialized in testsAppDelegate.mm on ios platform
-	//if device's screen width is greater than 768, use high definition images
-
-	if (screenSize.width >= 640) {
-		CCFileUtils::sharedFileUtils()->setResourceDirectory("iphone5");
-		pDirector->setContentScaleFactor(1136.0f / designSize.height);
-
-		//or else, use standard definition
-	} else {
-		CCFileUtils::sharedFileUtils()->setResourceDirectory("iphone");
-
-		pDirector->setContentScaleFactor(1024.0f / designSize.height);
-
-	}
-
-#else
-	//Other OS settings. TESTED OK in Android and BlackBerry 10
-
-	if (screenSize.height >= 1280) {
-		//High Resolution
-		searchPaths.push_back("hd");
-		pDirector->setContentScaleFactor(1280.0f / designSize.height);
-
-	} else if (screenSize.height >= 800) {
-		//Mid resolution
-		searchPaths.push_back("md");
-		pDirector->setContentScaleFactor(800.0f / designSize.height);
-
-	} else {
-		//Low resolution
-		searchPaths.push_back("sd");
-		pDirector->setContentScaleFactor(320.0f / designSize.height);
-
-	}
-#endif
-	cocos2d::CCFileUtils::sharedFileUtils()->setSearchPaths(searchPaths);
-	cocos2d::CCEGLView::sharedOpenGLView()->setDesignResolutionSize(designSize.width, designSize.height, kResolutionExactFit);
+    auto scene = HelloWorld::createScene();
 
     // run
-    pDirector->runWithScene(pScene);
+    director->runWithScene(scene);
 
     return true;
 }
 
-
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
-    cocos2d::CCDirector::sharedDirector()->stopAnimation();
+    Director::getInstance()->stopAnimation();
 
     // if you use SimpleAudioEngine, it must be pause
-    CocosDenshion::SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
-    cocos2d::CCDirector::sharedDirector()->startAnimation();
+    Director::getInstance()->startAnimation();
 
     // if you use SimpleAudioEngine, it must resume here
-    CocosDenshion::SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -2,32 +2,32 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/Backgrounds/Simple.cpp \
-                   ../../Classes/GameScene.cpp \
-                   ../../Classes/Common/SCScene.cpp \
-                   ../../Classes/Common/SCSprite.cpp \
-                   ../../Classes/Sprites/Balloon.cpp \
-                   ../../Classes/Sprites/Dryer.cpp 
+                   ../../Classes/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
-LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
-LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
-LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
-LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
-LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+# _COCOS_HEADER_ANDROID_BEGIN
+# _COCOS_HEADER_ANDROID_END
+
+
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
+# _COCOS_LIB_ANDROID_BEGIN
+# _COCOS_LIB_ANDROID_END
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,cocos2dx)
-$(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
-$(call import-module,CocosDenshion/android)
-$(call import-module,extensions)
-$(call import-module,external/Box2D)
-$(call import-module,external/chipmunk)
+$(call import-module,.)
+
+# _COCOS_LIB_IMPORT_ANDROID_BEGIN
+# _COCOS_LIB_IMPORT_ANDROID_END
